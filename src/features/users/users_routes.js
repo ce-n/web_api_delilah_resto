1a7acc9a316251a066815a18626380b0503933bb
[ce-n] <[ce.narambuena@gmail.com]>
2020-04-11 01:43:52 -0300

    [POST, PUT and DELETE of users]

--- src/features/users/users_routes.js
+++ src/features/users/users_routes.js
@@ -20,29 +20,52 @@ router
     .post(async(req, res) => {
         try {
             const user = req.body
-            await querys.insertNewProduct(sequelize, user)
-                //console.log(user)
+            await querys.insertNewUser(sequelize, user)
             res.status(201).json({ message: 'Successful operation. User created' })
         } catch (error) {
-            //console.log(error)
             res.status(500).json({ error: errorMessage, message: error })
         }
     })
-    .put((req, res) => {
-        try {
-            res.send('actualizar productos')
 
-        } catch (error) {
-            res.status(500).json({ error: errorMessage, message: error })
+router
+    .route('/:id')
+    .put(async(req, res) => {
+        const id = parseInt(req.params.id)
+
+        if (!isNaN(id)) {
+            try {
+                const userData = req.body
+                if (Object.entries(userData).length === 0) {
+                    res.sendStatus(400)
+                } else {
+                    await querys.updateUserByID(sequelize, id, userData)
+                    res.status(200).json({ message: 'User updated', user: userData })
+                }
+
+            } catch (error) {
+                res.sendStatus(500).json({ message: error })
+            }
+        } else {
+            res.sendStatus(400)
+            console.log('Id is not a number')
         }
+
     })
-    .delete((req, res) => {
-        try {
-            res.send('borrar producto')
+    .delete(async(req, res) => {
+        const id = parseInt(req.params.id)
+        if (!isNaN(id)) {
+            try {
+                await querys.deleteUserById(sequelize, id)
+                res.status(200).json({ message: 'The user has been deleted' })
+            } catch (error) {
+                res.status(500).json({ error: errorMessage, message: error })
+            }
 
-        } catch (error) {
-            res.status(500).json({ error: errorMessage, message: error })
+        } else {
+            res.sendStatus(400)
+            console.log('Id is not a number')
         }
+
     })
 
 module.exports = router
